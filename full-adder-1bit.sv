71b0cbdc70b31b78df6837a20bc0f6983db4392c
MUBEENAUQI <49668351+MUBEENAUQI@users.noreply.github.com>
2022-09-14 00:30:21 +0530

    Update full-adder-1bit.sv

--- full-adder-1bit.sv
+++ full-adder-1bit.sv
@@ -16,7 +16,7 @@ module fulladder(a,b,ci,s,c);
   wire T1,T2,T3;
   
   xor_gate X1(T1, a,b);
-  xor_gate X2(T2, T1,ci);
+  xor_gate X2(s, T1,ci);
   
   and_gate A2(T2, T1,ci);
   and_gate A1(T3, a,b);
@@ -38,8 +38,8 @@ module test;
   initial
     begin
       
-      $dumpfile("abc.vcd")
-      $dumbvars(1);
+      $dumpfile("mux.vcd"); //used to dump the waveforms
+      $dumpvars(1);
       $monitor("%d%d%d  %d,  %d",a,b,ci,s,c);
       #10 a=1'b0; b=1'b0; ci=1'b0;
       #10 a=1'b0; b=1'b0; ci=1'b1;
